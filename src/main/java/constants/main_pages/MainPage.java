179860ff1988bc73f9b53819bd61cf6e47a2ebb4
Max Kalugin <blindfoxkingdom@yandex.com>
2023-10-10 20:32:34 +0300

    tenth commit

---
+++ src/main/java/constants/main_pages/MainPage.java
@@ -0,0 +1,5 @@
+package constants.main_pages;
+
+public class MainPage {
+
+}
--- src/test/java/text_box_tests/Test1.java
+++ src/test/java/text_box_tests/Test1.java
@@ -1,5 +1,6 @@
 package text_box_tests;
 
+import browser.Browser;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
@@ -9,37 +10,50 @@ import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+import pages.base.BasePage;
+
 
 import java.time.Duration;
 
+import static constants.Constant.URLS.START_PAGE_URL;
+
 public class Test1 {
     private WebDriver driver;
-    private WebElement blockElements = driver.findElement(By.xpath("//*[@id=\"app\"]/div/div/div[2]/div/div[1]/div/div[3]/h5"));
-    private WebElement textBoxElements = driver.findElement(By.xpath("//*[@id=\"item-0\"]/span"));
-    private WebElement textResultElement = driver.findElement(By.xpath("//div[@class='col-12 mt-4 col-md-6']"));
+    private BasePage basePage;
+
+//    private WebElement blockElements = driver.findElement(By.xpath("//*[@id=\"app\"]/div/div/div[2]/div/div[1]/div/div[3]/h5"));
+//    private WebElement textBoxElements = driver.findElement(By.xpath("//*[@id=\"item-0\"]/span"));
+//    private WebElement textResultElement = driver.findElement(By.xpath("//div[@class='col-12 mt-4 col-md-6']"));
+
     @BeforeClass
     public void beforeClass(){
-        System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"\\drivers\\chromedriver.exe");
-        driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
-        driver.get("https://demoqa.com/");
-        driver.manage().window().maximize();
+        driver = Browser.createDriver();
+        driver.get(START_PAGE_URL);
+        basePage.openStartPage();
+
     }
+
     @Test
     public void step_01(){
 //        WebElement blockElements = driver.findElement(By.xpath("//*[@id=\"app\"]/div/div/div[2]/div/div[1]/div/div[3]/h5"));
 //        WebElement textBoxElements = driver.findElement(By.xpath("//*[@id=\"item-0\"]/span"));
+
+
+//        JavascriptExecutor js = (JavascriptExecutor) driver;
+//        js.executeScript("arguments[0].scrollIntoView()", blockElements);
+//        blockElements.click();
+        basePage.click("//*[@id=\\\"app\\\"]/div/div/div[2]/div/div[1]/div/div[3]/h5");
 //        WebElement textResultElement = driver.findElement(By.xpath("//div[@class='col-12 mt-4 col-md-6']"));
-        JavascriptExecutor js = (JavascriptExecutor) driver;
-        js.executeScript("arguments[0].scrollIntoView()", blockElements);
-        blockElements.click();
-        js.executeScript("arguments[0].scrollIntoView()", textBoxElements);
-        String text = textResultElement.getText();
+//        js.executeScript("arguments[0].scrollIntoView()", textBoxElements);
+//        String text = textResultElement.getText();
+        String text = basePage.getText("div[@class='col-12 mt-4 col-md-6");
         String textResult = "Please select an item from left to start practice.";
         Assert.assertTrue(text.equals(textResult));
 //        Assert.assertEquals(text,textResult);
 
+
     }
+
     @AfterClass
     public void afterClass(){
         driver.quit();
